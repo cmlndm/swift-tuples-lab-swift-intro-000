eb0b3b2227a52148be64e14d8c22f32dceb404dc
cmlndm <cmlndm@gmail.com>
2017-03-06 20:02:38 -0600

    Swift Tuples Lab

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
 // write your code here
-
+var person = ("Juan", 49)
 
 
 
@@ -27,7 +27,7 @@
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
 // write your code here
-
+print("\(person.0) is \(person.1) years old")
 
 
 
@@ -39,6 +39,7 @@
  ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
  */
 // write your code here
+person.1 += 1
 
 
 
@@ -51,7 +52,7 @@
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
 // write your code here
-
+print("Happy \(person.1) birthday, \(person.0)!")
 
 
 
@@ -63,7 +64,7 @@
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
 // write your code here
-
+var someone = (name: "Peter", age: 54)
 
 
 
@@ -75,7 +76,7 @@
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
 // write your code here
-
+print("\(someone.name) is \(someone.age) is years old")
 
 
 
@@ -86,7 +87,7 @@
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
 // write your code here
-
+someone.age += 1
 
 
 
@@ -97,7 +98,7 @@
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
 // write your code here
-
+print("Happy \(someone.age) birthday, \(someone.name)")
 
 
 
@@ -109,7 +110,7 @@
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
 // write your code here
-
+let human = (name: "John", age: 80)
 
 
 
@@ -120,7 +121,9 @@
 /*:
  ### 10. Can you change `human`'s age? Why or why not?
  */
-
+// Answer:
+// a) No, I cannot change human's age
+// b) Because is a human is a let constant
 
 
 
